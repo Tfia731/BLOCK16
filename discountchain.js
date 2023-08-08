41768bb97bc661b317f58ba0838b5c4dc2af5585
Toni <135765645+Tfia731@users.noreply.github.com>
2023-08-07 21:25:57 -0400

    Add files via upload

---
+++ discountchain.js
@@ -0,0 +1,65 @@
+const sarah = {
+    prescription: “diphenhydramine”,
+    pricePerRefill: 50,
+    refills: 1,
+    subscription: true,
+    coupon: false,
+  };
+  const rocky = {
+    prescription: “phenylephrine”,
+    pricePerRefill: 30,
+    refills: 5,
+    subscription: true,
+    coupon: true,
+  };
+
+  /*
+  If a customer has a subscription, the customer will receive a 25% discount after the refill total has been calculated.
+  If the customer has a coupon, the customer will receive a $10 discount after the subscription discount has been calculated.
+  At the end of the script, you should return and log the string “Your grand total is ${finalAmount}.”
+  */
+  // need price before discount - pricePerRefill x refills
+  // if sub is true, 25% discount
+  // if coupon is true, - $10
+
+  
+  const timmy = {
+    prescription: “acetaminophen”,
+    pricePerRefill: 25,
+    refills: 3,
+    subscription: false,
+    coupon: true,
+  };
+  function pricebeforeDiscount(pricePerRefill, refills) {
+    return pricePerRefill * refills;
+  }
+  function priceSubscription(pricebeforeDiscount, subscription) {
+    if (subscription) {
+      return pricebeforeDiscount - pricebeforeDiscount * 0.25;
+    } else {
+      return pricebeforeDiscount;
+    }
+  }
+  //
+  function priceDiscountCoupon(priceSubscription, coupon){
+      if (coupon){
+          return priceSubscription - 10;
+      } else {
+          return priceSubscription;
+      }
+  }
+  const timmy1 = pricebeforeDiscount(timmy.pricePerRefill, timmy.refills);
+  const timmy2 = priceSubscription(timmy1, timmy.subscription);
+  const timmy3 = priceDiscountCoupon(timmy2, timmy.coupon);
+  console.log(timmy3);
+
+  const sarah1 = pricebeforeDiscount(sarah.pricePerRefill, sarah.refills);
+  const sarah2 = priceSubscription(sarah, sarah.subscription);
+  const sarah3 = priceDiscountCoupon(sarah2, sarah.coupon);
+  console.log(sarah3);
+
+  const rocky1 = priceBeforeDiscount(rocky.pricePerRefill, rocky.refills);
+  const rocky2 = priceSubscription(rocky1, rocky.subscription);
+  const rocky3 = priceDiscountCoupon(rocky2, rocky.coupon);
+  console.log(rocky3);
+ 
